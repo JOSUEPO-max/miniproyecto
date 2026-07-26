d3c5848b339fbc790b092ddb5750a6f08d1eff2b
Josué Marcillo <edwin.marcillo@tecnologicoliceocristiano.edu.ec>
2026-07-26 16:30:55 -0500

    Agregar archivos de proyecto.

---
+++ miniproyecto/Categoria.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace miniproyecto
+{
+    namespace MiniProyecto
+    {
+        public class Categoria
+        {
+            // Atributos privados
+            private int _id;
+            private string _nombre;
+
+            // Constructores
+            public Categoria() { }
+
+            public Categoria(int id, string nombre)
+            {
+                _id = id;
+                _nombre = nombre;
+            }
+
+            // Propiedades
+            public int Id
+            {
+                get { return _id; }
+                set { _id = value; }
+            }
+
+            public string Nombre
+            {
+                get { return _nombre; }
+                set { _nombre = value; }
+            }
+        }
+    }
---
+++ miniproyecto/Producto.cs
@@ -0,0 +1,59 @@
+﻿using miniproyecto.MiniProyecto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace miniproyecto
+{
+    public class Producto
+    {
+        // Atributos privados
+        private string _codigo;
+        private string _nombre;
+        private double _precio;
+        private Categoria _categoria;
+
+        // Constructor vacío
+        public Producto() { }
+
+        // Constructor parametrizado
+        public Producto(string codigo, string nombre, double precio, Categoria categoria)
+        {
+            _codigo = codigo;
+            _nombre = nombre;
+            _precio = precio;
+            _categoria = categoria;
+        }
+
+        // Propiedades
+        public string Codigo
+        {
+            get { return _codigo; }
+            set { _codigo = value; }
+        }
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public double Precio
+        {
+            get { return _precio; }
+            set { _precio = value; }
+        }
+
+        public Categoria Categoria
+        {
+            get { return _categoria; }
+            set { _categoria = value; }
+        }
+
+        // Método auxiliar para dar formato de texto
+        public string ObtenerResumen()
+        {
+            return $"{Codigo} | {Nombre} | ${Precio} | Cat: {Categoria.Nombre}";
+        }
+    }
+}
